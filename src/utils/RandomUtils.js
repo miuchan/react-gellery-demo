7d5c1d2b9ce94de034d500dad6449640700d2e74
miuchan <k737566@gmail.com>
2016-10-20 22:59:24 +0800

    add dist

--- src/utils/RandomUtils.js
+++
@@ -1,8 +0,0 @@
-export function getRamdomValueInRange(low, high) {
-  return Math.ceil(Math.random() * (high - low) + low);
-}
-
-export function getRandomDeg() {
-  let deg = Math.ceil(Math.random() * 30);
-  return Math.random() > 0.5 ? deg : -deg;
-}
